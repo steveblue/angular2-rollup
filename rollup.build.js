099c028afe5aa291013201104546b1322ddcd7e3
Steve Belovarich <steveblue@gmail.com>
2016-09-23 21:56:46 -0700

    Updated build to support @types, included windows fix for Rollup, bumped version of testing packages

--- rollup.build.js
+++ rollup.build.js
@@ -10,6 +10,14 @@ const cleanup = require('rollup-plugin-cleanup');
 var bundle = rollup.rollup({
     entry: './main.prod.js',
     plugins: [
+      {
+        resolveRxJS: id => {
+          if (id.startsWith('rxjs/') || id.startsWith('rxjs\\')) {
+            let result = `${__dirname}/node_modules/rxjs-es/${id.replace('rxjs/', '')}.js`;
+            return result.replace(/\//g, "\\");
+          }
+        }
+      },
       alias({ rxjs: __dirname + '/node_modules/rxjs-es' }),
       resolve({ module: true }),
       replace({ 'ENVIRONMENT': JSON.stringify( 'production' ) }),
