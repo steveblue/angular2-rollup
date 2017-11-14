8d0430a3470e591ddd376b415427c59d19ee07bc
Steve Belovarich <steveblue@gmail.com>
2017-11-14 14:23:26 -0800

    add sourcemaps to umd and es5 bundles created by library build

--- build.lib.js
+++ build.lib.js
@@ -193,6 +193,7 @@ const compile = {
             alert('Babel', 'started transpiling', config.libFilename + '.umd.js');
 
             let transpile = exec(path.normalize(config.processRoot + '/node_modules/.bin/babel') +
+              ' --source-maps' + 
               ' --presets=es2015-rollup ' +
               ' --plugins=transform-es2015-modules-commonjs ' +
               ' --module umd ' +
@@ -262,6 +263,7 @@ const compile = {
             alert('Babel', 'started transpiling', config.libFilename + '.es5.js');
 
             let transpile = exec(path.normalize(config.processRoot + '/node_modules/.bin/babel') +
+              ' --source-maps' + 
               ' --presets=es2015-rollup ' + path.normalize('./dist/') + config.libFilename + '.es5.js' +
               ' --out-file ' + path.normalize('./dist/') + config.libFilename + '.es5.js', function (code, output, error) {
                 alert('Babel', 'transpiled', './' + config.dist + '/' + config.libFilename + ' to', './' + config.dist + '/' + config.libFilename + '.es5.js');
