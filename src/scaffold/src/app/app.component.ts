9ed0d373b4ed83c58a4e85c31b5496163d8be142
Steve Belovarich <steveblue@gmail.com>
2018-04-05 21:32:48 -0700

    return moduleId for JIT compilation

--- src/scaffold/src/app/app.component.ts
+++ src/scaffold/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+    moduleId: module.id,
     selector: 'cmp-prefix1-app-root',
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.css']
