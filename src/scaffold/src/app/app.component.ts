3c695b2a79d34b6c09e6c8858431a5eec070c50d
Steve Belovarich <steveblue@gmail.com>
2018-06-08 23:22:14 -0700

    sync app-root in scaffolded app

--- src/scaffold/src/app/app.component.ts
+++ src/scaffold/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'cmp-prefix1-app-root',
+    selector: 'app-root',
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.css']
 })
