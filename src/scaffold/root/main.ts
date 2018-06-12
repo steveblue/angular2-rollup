d70792abf821a172342c2abf60b65596ea7a3235
Steve Belovarich <steveblue@gmail.com>
2018-06-12 14:43:43 -0700

    fix issues with ngfactory naming

--- src/scaffold/root/main.ts
+++ src/scaffold/root/main.ts
@@ -1,5 +1,5 @@
 import { platformBrowser } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
-import { AppModuleNgFactory } from './out-tsc/src/app/app.module.out-tsc';
+import { AppModuleNgFactory } from './out-tsc/src/app/app.module.ngfactory';
 enableProdMode();
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
--- src/scaffold/src/app/app.module.ts
+++ src/scaffold/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule }      from '@angular/core';
 import { CommonModule }  from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent }  from './app.component';
@@ -13,7 +12,6 @@ import { LazyModule } from './shared/components/lazy/lazy.module';
     imports: [ BrowserModule,
                BrowserAnimationsModule,
                CommonModule,
-               HttpClientModule,
                HomeModule,
                LazyModule,
                routing],
