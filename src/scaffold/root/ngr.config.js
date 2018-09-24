7a118b7a583a7316106c12e2021ca298017df16b
Steve Belovarich <steveblue@gmail.com>
2018-09-24 16:56:17 -0700

    UPDATE ngr.config.js for new scaffolded apps

--- src/scaffold/root/ngr.config.js
+++ src/scaffold/root/ngr.config.js
@@ -2,6 +2,61 @@ const spawn = require('child_process').spawn;
 const fs = require('fs');
 
 module.exports = {
+    projects: {
+        '{{projectName}}': {
+            root: '',
+            sourceRoot: 'src',
+            projectType: 'application',
+            configFile: '',
+            architect: {
+                build: {
+                    builder: 'angular-rollup',
+                    options: {
+                        outputPath: 'dist/{{projectName}}',
+                        styles: ['src/style/style.scss'],
+                        lib: {
+                            dev: [
+                                "core-js/client/shim.min.js",
+                                "core-js/client/shim.min.js.map",
+                                "zone.js/dist/zone.min.js",
+                                "web-animations-js/web-animations.min.js",
+                                "web-animations-js/web-animations.min.js.map",
+                                "ie9-oninput-polyfill/ie9-oninput-polyfill.js",
+                                "angular-polyfills/dist/blob.js",
+                                "angular-polyfills/dist/classList.js",
+                                "angular-polyfills/dist/formdata.js",
+                                "angular-polyfills/dist/intl.js",
+                                "angular-polyfills/dist/typedarray.js",
+                                "console-polyfill/index.js",
+                                "systemjs/dist/system.js",
+                                "systemjs/dist/system.js.map",
+                                "reflect-metadata/Reflect.js",
+                                "reflect-metadata/Reflect.js.map",
+                                "tslib/tslib.js",
+                                "@angular",
+                                "rxjs"
+                            ],
+                            prod: [
+                                "core-js/client/shim.min.js",
+                                "zone.js/dist/zone.min.js",
+                                "web-animations-js/web-animations.min.js",
+                                "ie9-oninput-polyfill/ie9-oninput-polyfill.js",
+                                "angular-polyfills/dist/blob.js",
+                                "angular-polyfills/dist/classList.js",
+                                "angular-polyfills/dist/formdata.js",
+                                "angular-polyfills/dist/intl.js",
+                                "angular-polyfills/dist/typedarray.js",
+                                "console-polyfill/index.js",
+                                "systemjs/dist/system.js"
+                            ],
+                            src: "node_modules",
+                            dist: "dist/{{projectName}}/lib"
+                        }
+                    }
+                }
+            }
+        }
+    },
     buildHooks: {
         prod: {
             pre: () => {
@@ -17,44 +72,6 @@ module.exports = {
                 });
             }
         }
-    },
-    lib: {
-        dev: [
-            "core-js/client/shim.min.js",
-            "core-js/client/shim.min.js.map",
-            "zone.js/dist/zone.min.js",
-            "web-animations-js/web-animations.min.js",
-            "web-animations-js/web-animations.min.js.map",
-            "ie9-oninput-polyfill/ie9-oninput-polyfill.js",
-            "angular-polyfills/dist/blob.js",
-            "angular-polyfills/dist/classList.js",
-            "angular-polyfills/dist/formdata.js",
-            "angular-polyfills/dist/intl.js",
-            "angular-polyfills/dist/typedarray.js",
-            "console-polyfill/index.js",
-            "systemjs/dist/system.js",
-            "systemjs/dist/system.js.map",
-            "reflect-metadata/Reflect.js",
-            "reflect-metadata/Reflect.js.map",
-            "tslib/tslib.js",
-            "@angular",
-            "rxjs"
-        ],
-        prod: [
-            "core-js/client/shim.min.js",
-            "zone.js/dist/zone.min.js",
-            "web-animations-js/web-animations.min.js",
-            "ie9-oninput-polyfill/ie9-oninput-polyfill.js",
-            "angular-polyfills/dist/blob.js",
-            "angular-polyfills/dist/classList.js",
-            "angular-polyfills/dist/formdata.js",
-            "angular-polyfills/dist/intl.js",
-            "angular-polyfills/dist/typedarray.js",
-            "console-polyfill/index.js",
-            "systemjs/dist/system.js"
-        ],
-        src: "node_modules",
-        dist: "dist/{{projectName}}/lib"
     },
     style: {
         sass: {
