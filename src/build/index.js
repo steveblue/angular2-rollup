157917396563479b5bd68a3f2925847b0736d2be
Steve Belovarich <steveblue@gmail.com>
2018-09-24 14:50:27 -0700

    FIXED logs and added warning for existing projects not updated to latest schema

--- src/build/index.js
+++ src/build/index.js
@@ -3,14 +3,24 @@ const colors = require('colors');
 const log = require('./../log.js');
 const cli = require('./../../cli.config.json');
 const config = require('./../config');
+const path = require('path');
 
 class Build {
   constructor() {
+
+    if (!config.projects) {
+      log.error('NGR ERROR: ngr >= 2.0.0 requires projects schema in ngr.config.js');
+    }
     this.outputPath = config.projects[config.project].architect.build.options.outputPath;
     this.startTime = moment(new Date());
     if (!cli.program.webpack) {
-      log.alert(colors.green('build start'));
-      log.alert(colors.dim('env ' + cli.env));
+      const args = cli.program.rawArgs.filter((str) => {
+        return !str.includes('node');
+      }).map((str) => {
+        return path.basename(str);
+      });
+      log.alert(colors.green('ngr ' + args.join(' ')));
+
     }
   }
 }
